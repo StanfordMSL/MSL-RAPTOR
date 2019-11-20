3b6511cb0caf370858a9853a8967eead09d19b39
Adam Caccavale <adam.w.caccavale@gmail.com>
2019-11-19 18:58:39 -0800

    Updated ros_interface code to subscribe to the camera images and the ekf pose value. The closest pose to the given image is found.

--- src/msl_raptor.py
+++ src/msl_raptor.py
@@ -17,23 +17,36 @@ import numpy as np
 # import matplotlib
 # from matplotlib import pyplot as plt
 # from mpl_toolkits import mplot3d
+# ros
+import rospy
 # libs
 from ros_interface import ros_interface as ROS
 
 def run_execution_loop():
+    rate = rospy.Rate(100) # max filter rate
     b_target_in_view = True
-    ros = ROS()
+    last_image_time = -1
+    ros = ROS()  # create a ros interface object
 
     loop_count = 0
-    while b_target_in_view:
-        # read in latest data
+    while not rospy.is_shutdown():
+        if ros.latest_time <= last_image_time:
+            # this means we dont have new data yet
+            continue
+
+        rospy.loginfo("Recieved new image at time {:.4f}".format(ros.latest_time))
+
         # update ukf
         # [optional] update plots
+        rate.sleep()
         loop_count += 1
-        if loop_count > 100:
-            break
+
 
 if __name__ == '__main__':
     print("Starting MSL-RAPTOR main")
+    rospy.init_node('RAPTOR_MSL', anonymous=True)
     run_execution_loop()
     print("--------------- FINISHED ---------------")
+
+
+            
--- src/ros_interface.py
+++ src/ros_interface.py
@@ -3,51 +3,114 @@
 import sys, time #, argparse
 # math
 import numpy as np
+from bisect import bisect_left
 # ros
 import rospy
+from geometry_msgs.msg import PoseStamped, Twist, Pose
+from sensor_msgs.msg import Image, CameraInfo
 from std_msgs.msg import Float32MultiArray, MultiArrayDimension
 
 
 class ros_interface:
 
     def __init__(self):
-        rospy.init_node('bounding_box_network_interface', anonymous=True)
+        
         self.VERBOSE = True
 
         # Paramters #############################
         self.b_detect_new_bb = True  # set to false if last frame we had a bb (if false, use a tracking network like SiamMask)
         self.latest_bb = None
-        self.latest_time = 0.0
         self.latest_ego_pose = None
-        self.latest_bb_method = -1  # -1 for detect network, 1 for tracking network
+        self.latest_bb_method = 1  # 1 for detect network, -1 for tracking network
+        self.latest_time = 0
+        self.image_dims = None
+        self.pose_buffer = ([], [])
+        self.pose_queue_size = 50
+        self.start_time = 0
         ####################################################################
-        
-        # Subscribers / Listeners & Publishers #############################
-        # self.trans_listener = tf.TransformListener()
-        # rospy.Subscriber('/IMAGE_TOPIC', IMAGE_MSG, self.NN_CALLBACK)
-        self.bb_pub = rospy.Publisher('/bb', Float32MultiArray, queue_size=5)
-        ####################################################################
-        
-        # create dummy message to send #############################
-        data_len = 5
-        self.dummy_bb_msg = Float32MultiArray()
-        self.dummy_bb_msg.layout.dim.append(MultiArrayDimension())
-        self.dummy_bb_msg.layout.dim.append(MultiArrayDimension())
-        self.dummy_bb_msg.layout.dim[0].size = data_len
-        self.dummy_bb_msg.layout.dim[1].size = 1
-        self.dummy_bb_msg.layout.dim[0].stride = data_len*1
-        self.dummy_bb_msg.layout.dim[1].stride = data_len
-        self.dummy_bb_msg.layout.dim[0].label = "rows"
-        self.dummy_bb_msg.layout.dim[1].label = "cols"
-        self.dummy_bb_msg.layout.data_offset = 0
-        self.dummy_bb_msg.data = [self.latest_time, 120, 230, 40, 20, 10*np.pi/180, self.latest_bb_method]
-        ####################################################################
-        
-        self.run()
 
 
-    def run(self):
-        rate = rospy.Rate(30)
-        while not rospy.is_shutdown():
-            self.bb_pub.publish(self.dummy_bb_msg)
-            rate.sleep()
+        self.ns = rospy.get_param('~ns')  # robot namespace
+        
+        # Subscribers / Listeners & Publishers #############################        
+        camera_info = rospy.wait_for_message(self.ns + '/camera/camera_info', CameraInfo, 5)
+        self.start_time = camera_info.header.stamp.to_sec()
+        self.K = camera_info.K
+
+
+        rospy.Subscriber(self.ns + '/camera/image_raw', Image, self.image_cb)
+        rospy.Subscriber(self.ns + '/mavros/local_position/pose', PoseStamped, self.pose_ekf_cb, queue_size=10)
+        # rospy.Subscriber(self.ns + '/mavros/vision_position/pose', PoseStamped, self.pose_gt_cb, queue_size=10)
+        ####################################################################
+        
+
+
+    def pose_ekf_cb(self, msg):
+        """
+        Maintains a buffer of poses and times. The first element is the earliest. 
+        Stored in a way to interface with a quick method for finding closest match by time.
+        """
+
+        time = self.get_ros_time()  # time in seconds
+
+        if len(self.pose_buffer[0]) < self.pose_queue_size:
+            self.pose_buffer[0].append(msg.pose)
+            self.pose_buffer[1].append(time)
+        else:
+            self.pose_buffer[0][0:self.pose_queue_size] = self.pose_buffer[0][1:self.pose_queue_size]
+            self.pose_buffer[1][0:self.pose_queue_size] = self.pose_buffer[1][1:self.pose_queue_size]
+            self.pose_buffer[0][-1] = msg.pose
+            self.pose_buffer[1][-1] = time
+
+
+    def image_cb(self, msg):
+        """
+        recieve an image, process w/ NN, then set variables that the main function will access 
+        note: set the time variable at the end (this signals the program when new data has arrived)
+        """
+        if not self.image_dims:
+            self.image_dims = (msg.height, msg.width)
+
+        if len(self.pose_buffer[0]) == 0:
+            return # this happens if we are just starting
+
+        time = msg.header.stamp.to_sec() - self.start_time  # timestamp in seconds
+        self.latest_ego_pose = self.find_closest_by_time(time)
+
+        # call NN here!!!!
+        image = msg.data
+        if self.b_detect_new_bb:
+            self.latest_bb_method = 1
+        else:
+            self.latest_bb_method = -1
+            
+        self.latest_bb = [120, 230, 40, 20, 10*np.pi/180]
+        self.latest_time = time  # DO THIS LAST
+
+
+    def get_ros_time(self):
+        """
+        returns ros time in seconds (minus time at start of program)
+        """
+        ts = rospy.Time.now()
+        return ts.to_sec() - self.start_time
+
+
+    def find_closest_by_time(self, time_to_match):
+        """
+        Assumes my_list is sorted. Returns closest item in list by time. If two numbers are equally close, return the smallest number.
+        Adapted from https://stackoverflow.com/questions/12141150/from-list-of-integers-get-number-closest-to-a-given-value/12141511#12141511
+        """
+        pose_list = self.pose_buffer[0]
+        time_list = self.pose_buffer[1]
+        pos = bisect_left(time_list, time_to_match)
+        if pos == 0:
+            return pose_list[0], 0
+        if pos == len(myList):
+            return pose_list[-1], len(pose_list) - 1
+        before = time_list[pos - 1]
+        after = time_list[pos]
+        if after - time_to_match < time_to_match - before:
+           return pose_list[pos], pos
+        else:
+           return pose_list[pos - 1], pos - 1
