Added code to find mean of quaternions. Fixed some misc functions to keep first element of quaternions positive. Added function to turn quat into axis angle

src/ukf.py
@@ -2,6 +2,7 @@
 # IMPORTS
 #tools
 from copy import copy
+import pdb
 # math
 import numpy as np
 import numpy.linalg as la
@@ -33,8 +34,10 @@ class UKF:
         self.sig_pnt_multiplier = np.sqrt(self.dim_sig + ukf_lambda)
 
         self.w0_m = ukf_lambda / (ukf_lambda + self.dim_sig)
-        self.w0_c = self.w0_m + (1 - alpha**22 + beta)
-        self.wi = 1 / (2*  (ukf_lambda + self.dim_sig))
+        self.w0_c = self.w0_m + (1 - alpha**2 + beta)
+        self.wi = 1 / (2 * (ukf_lambda + self.dim_sig))
+        self.w_arr = np.ones((1+ 2 * self.dim_sig,)) * self.wi
+        self.w_arr[0] = self.w0_m
 
         self.camera = {}
         ####################################################################
@@ -55,12 +58,13 @@ class UKF:
 
     def step_ukf(self, measurement, bb_3d, tf_ego_w, dt):
         self.ukf_itr += 1
-
         sps = self.calc_sigma_points()
         sps_prop = np.empty_like(sps)
         for sp_ind in range(sps_prop.shape[1]):
             sps_prop[:, sp_ind] = self.propagate_dynamics(sps[:, sp_ind], dt)
 
+        mu_bar, sig_bar = self.extract_mean_and_cov_from_state_sigma_points(sps_prop)
+        pdb.set_trace()
     
     def calc_sigma_points(self):
         sps = np.zeros((self.dim_state, 2 * self.dim_sig + 1))
@@ -103,10 +107,13 @@ class UKF:
         quat = state[6:10]  # current orientation
         omegas = state[10:13]  # angular velocity vector
         om_norm = la.norm(omegas)  # rate of change of all angles
-        ang = om_norm * dt  # change in angle in this small timestep
-        ax = omegas / om_norm  # axis about angle change
-        quat_delta = axang_to_quat(ax * ang)
-        quat_new = quat_mul(quat_delta, quat)
+        if om_norm > 0:
+            ang = om_norm * dt  # change in angle in this small timestep
+            ax = omegas / om_norm  # axis about angle change
+            quat_delta = axang_to_quat(ax * ang)
+            quat_new = quat_mul(quat_delta, quat)
+        else:
+            quat_new = quat
 
         [roll, pitch, yaw] = quat_to_ang(quat_new)
         if self.b_enforce_0_yaw:
@@ -116,8 +123,11 @@ class UKF:
         return next_state
 
     
-    def extract_mean_and_cov_from_state_sigma_points():
-        pass
+    def extract_mean_and_cov_from_state_sigma_points(self, sps):
+        mu_bar = self.w0_m * sps[:, 0] + self.wi*np.sum(sps[:, 1:], 1)
+        mu_bar[6:10] = average_quaternions(sps[6:10, :].T, self.w_arr)
+        sig_bar = 0
+        return mu_bar, sig_bar
         
 
     def extract_mean_and_cov_from_obs_sigma_points():

src/utils/ros_utils.py
@@ -28,7 +28,7 @@ def pose_to_state_vec(pose):
     """ Turn a ROS pose message into a 13 el state vector (w/ 0 vels) """
     state = np.zeros((13,))
     state[0:3] = [pose.position.x, pose.position.y, pose.position.z]
-    state[6:10] = [pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w]
+    state[6:10] = [pose.orientation.w, pose.orientation.x, pose.orientation.y, pose.orientation.z]
     if state[6] < 0:
         state[6:10] *= -1
     return state

src/utils/ukf_utils.py
@@ -1,16 +1,19 @@
 ####### ROS UTILITIES #######
 # IMPORTS
+# tools
+import pdb
 # math
 import numpy as np
 import numpy.linalg as la
 import tf
+
 
 def axang_to_quat(axang):
     """ 
     takes in an orientation in axis-angle form s.t. |axang| = ang, and 
     axang/ang = unit vector about which the angle is rotated. Returns a quaternion
     """
-    quat = np.array([1, 0, 0, 0])
+    quat = np.array([1., 0., 0., 0.])
     ang = la.norm(axang)
 
     # deal with numeric issues
@@ -21,16 +24,48 @@ def axang_to_quat(axang):
     
     return quat
 
+
+def quat_to_axang(quat):
+    """ 
+    takes in an orientation in axis-angle form s.t. |axang| = ang, and 
+    axang/ang = unit vector about which the angle is rotated. Returns a quaternion
+    """
+    axang = np.zeros((3,))
+    # deal with some numerical issues...
+    if abs(quat[0]) > 1:
+        if abs(la.norm(quat) - 1) < 0.0001:
+            # this is a numeric issue (w/ no rotation)
+            axang = np.zeros((3,))
+        else:
+            raise RuntimeException("Invalid quaternion!")
+
+    ang = 2 * np.arccos(quat[0])
+
+    if abs(ang) < 0.0001:
+        return  # no rotation, return zeros
+
+    ax = quat[1:4] / la.norm(quat[1:4])  # unit vector
+    return ang * ax
+
+
+def quat_inv(q):
+    """ technically this is the conjugate, for unit quats this is same as inverse """
+    return np.array([-q[0], q[1], q[2], q[3]])
+
+
 def quat_mul(q0, q1):
     """
     multiply q0 by q1. first element in quat is scalar value
     """
     w0, x0, y0, z0 = q0
     w1, x1, y1, z1 = q1
-    return np.array([-x1 * x0 - y1 * y0 - z1 * z0 + w1 * w0,
-                      x1 * w0 + y1 * z0 - z1 * y0 + w1 * x0,
-                     -x1 * z0 + y1 * w0 + z1 * x0 + w1 * y0,
-                      x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0], dtype=np.float64)
+    q_out = np.array([-x1 * x0 - y1 * y0 - z1 * z0 + w1 * w0,
+                       x1 * w0 + y1 * z0 - z1 * y0 + w1 * x0,
+                      -x1 * z0 + y1 * w0 + z1 * x0 + w1 * y0,
+                       x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0], dtype=np.float64)
+    if q_out[0] < 0:
+        q_out *= -1
+    return q_out
 
 def quat_to_ang(q):
     """
@@ -49,3 +84,54 @@ def ang_to_quat(angs):
     q = tf.transformations.quaternion_from_euler(angs[0], angs[1], angs[2], 'rxyz')
     return np.array([q[3], q[0], q[1], q[2]])
 
+
+def calc_mean_weighted_quat(quat_arr):
+    """
+    find mean of quat_arr which should be N x 4
+    """
+    q_mean = quat_arr[0, :]
+    
+
+def average_quaternions(Q, w=None):
+    """
+    Adapted from https://github.com/christophhagen/averaging-quaternions/blob/master/averageQuaternions.py
+    Q is a Nx4 numpy matrix and contains the quaternions to average in the rows.
+    The quaternions are arranged as (w,x,y,z), with w being the scalar
+    The result will be the average quaternion of the input. Note that the signs
+    of the output quaternion can be reversed, since q and -q describe the same orientation
+    The weight vector w must be of the same length as the number of rows in the quaternion maxtrix Q
+    """
+    M = Q.shape[0]  # Number of quaternions to average
+    
+    if w is None:
+        w = np.ones((M,)) / M  # DEFAULT: equally weighted
+
+    A = np.zeros((4,4))
+    weightSum = 0
+
+    for i in range(0,M):
+        q = Q[i,:]
+        A = w[i] * np.outer(q,q) + A
+        weightSum += w[i]
+
+    # scale
+    A = (1.0/weightSum) * A
+
+    # compute eigenvalues and -vectors
+    eigenValues, eigenVectors = la.eig(A)
+
+    # Sort by largest eigenvalue
+    eigenVectors = eigenVectors[:,eigenValues.argsort()[::-1]]
+
+    # return the real part of the largest eigenvector (has only real part)
+    q_mean = np.real(eigenVectors[:,0])
+    if q_mean[0] < 0:
+        q_mean *= -1
+    # calc set of differences from each quat to the mean (in ax-angle rep.)
+    ei_vec_set = np.zeros((3, M)) 
+    q_mean_inv = quat_inv(q_mean)
+    for i, qi in enumerate(Q):
+        ei_quat = quat_mul(qi, q_mean_inv)
+        ei_vec_set[:, i] = quat_to_axang(ei_quat)
+
+    return q_mean, ei_vec_set