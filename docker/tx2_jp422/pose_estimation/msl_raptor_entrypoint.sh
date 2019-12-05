75035a0294ce69ba53ed1a60ef5087617620cc2e
Adam Caccavale <adam.w.caccavale@gmail.com>
2019-12-05 12:46:42 -0800

    replaced hyphen with underscore in file name

--- docker/tx2_jp422/pose_estimation/msl_raptor_entrypoint.sh
+++ docker/tx2_jp422/pose_estimation/msl_raptor_entrypoint.sh
@@ -3,5 +3,5 @@ set -e
 
 # setup ros environment
 source "/root/msl_raptor_ws/devel/setup.bash"
-export PYTHONPATH="/root/msl_raptor_ws/src/msl_raptor/src/front-end/SiamMask:$PYTHONPATH:/root/msl_raptor_ws/src/msl_raptor/src/front-end/yolov3:/root/msl_raptor_ws/src/msl_raptor/src/front-end/SiamMask/experiments/siammask_sharp:/root/python3_ws/install/lib/python3/dist-packages"
+export PYTHONPATH="/root/msl_raptor_ws/src/msl_raptor/src/front_end/SiamMask:$PYTHONPATH:/root/msl_raptor_ws/src/msl_raptor/src/front_end/yolov3:/root/msl_raptor_ws/src/msl_raptor/src/front_end/SiamMask/experiments/siammask_sharp:/root/python3_ws/install/lib/python3/dist-packages"
 exec "$@"
