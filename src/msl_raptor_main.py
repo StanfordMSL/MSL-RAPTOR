Fix free scaling parameter used in main code to undistort

src/msl_raptor_main.py
@@ -179,7 +179,7 @@ class camera:
         camera_info = rospy.wait_for_message(ns + '/camera/camera_info', CameraInfo, 10)
         self.K = np.reshape(camera_info.K, (3, 3))
         self.dist_coefs = np.reshape(camera_info.D, (5,))
-        self.new_camera_matrix, _ = cv2.getOptimalNewCameraMatrix(self.K, self.dist_coefs, (camera_info.width, camera_info.height), 1, (camera_info.width, camera_info.height))
+        self.new_camera_matrix, _ = cv2.getOptimalNewCameraMatrix(self.K, self.dist_coefs, (camera_info.width, camera_info.height), 0, (camera_info.width, camera_info.height))
 
         self.K_inv = la.inv(self.K)
         self.new_camera_matrix_inv = la.inv(self.new_camera_matrix)