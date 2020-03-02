a4ba90adc4469251c552a95bce5a1c2deffc35e7
Adam <adam.w.caccavale@gmail.com>
2020-03-02 05:14:42 +0000

    draw teh true measurement in addition to what measurement would be generated if the state were true on 3d bb overlay image

--- src/viz_tools/poses_viz_node.py
+++ src/viz_tools/poses_viz_node.py
@@ -8,7 +8,7 @@ import pdb
 import numpy as np
 # ros
 import rospy
-from msl_raptor.msg import TrackedObjects, TrackedObject
+from msl_raptor.msg import AngledBbox, AngledBboxes, TrackedObjects, TrackedObject
 from sensor_msgs.msg import CameraInfo
 from sensor_msgs.msg import Image as ROSImage
 from geometry_msgs.msg import Pose, PoseStamped, PoseArray
@@ -29,9 +29,14 @@ class poses_viz_node:
         rospy.init_node('poses_viz_node', anonymous=True)
 
         self.ns = rospy.get_param('~ns')  # robot namespace
+
+        b_draw_2d_bbs_on_3dbb_proj = True
        
         self.tracked_objects_sub = rospy.Subscriber(self.ns + '/msl_raptor_state', TrackedObjects, self.tracked_objects_cb, queue_size=5)
         self.pose_array_pub = rospy.Publisher(self.ns + '/tracked_objects_poses', PoseArray, queue_size=5)
+        self.bb_data_sub = rospy.Subscriber(self.ns + '/bb_data', AngledBboxes, self.bb_viz_cb, queue_size=5)
+        self.bb_msg_buf = ([], [])
+        self.bb_msg_buf_maxlen = 50
         
         # for displaying the 3d bb projected onto an image
         self.b_overlay = rospy.get_param('~b_overlay')  # robot namespace
@@ -98,14 +103,43 @@ class poses_viz_node:
                 else:
                     image = draw_2d_proj_of_3D_bounding_box(image, corners2D_pr=proj_3d_bb)
                 
-                abb = verts_to_angled_bb(proj_3d_bb)
-                box = np.int0(cv2.boxPoints( ( (abb[0], abb[1]), (abb[2], abb[3]), -np.degrees(abb[4]))) )
-                cv2.drawContours(image, [box], 0, (0,255,0), 2)
+                if b_draw_2d_bbs_on_3dbb_proj:
+                    abb = verts_to_angled_bb(proj_3d_bb)
+                    box = np.int0(cv2.boxPoints( ( (abb[0], abb[1]), (abb[2], abb[3]), -np.degrees(abb[4]))) )
+                    cv2.drawContours(image, [box], 0, (255,0,0), 2)
+                    
+                    bb_msg, _ = find_closest_by_time(my_time, self.bb_msg_buf[1], self.bb_msg_buf[0])
+                    box = np.int0(cv2.boxPoints( ( (bb_msg.x, bb_msg.y), (bb_msg.width, bb_msg.height), -np.degrees(bb_msg.angle))) )
+                    cv2.drawContours(image, [box], 0, (0,255,0), 2)
             
 
         self.pose_array_pub.publish(pose_arr)
         if self.b_overlay:
             self.img_overlay_pub.publish(self.bridge.cv2_to_imgmsg(image, "bgr8"))
+
+
+    def bb_viz_cb(self, bb_list_msg):
+        """
+        custom list of angled bb message type:
+            Header header  # ros timestamp etc
+            float64 x  # x coordinate of center of bounding box
+            float64 y  # y coordinate of center of bounding box
+            float64 width  # width (long axis) of bounding box
+            float64 height  # height (short axis) of bounding box
+            float64 angle  # angle in radians
+            uint8 im_seg_mode  # self.DETECT = 1  self.TRACK = 2  self.FAKED_BB = 3  self.IGNORE = 4
+        """
+        # my_time = rospy.Time.now().to_sec()  # time in seconds
+        my_time = bb_list_msg.header.stamp.to_sec()  # time in seconds
+
+        if len(self.bb_msg_buf[0]) < self.bb_msg_buf_maxlen:
+            self.bb_msg_buf[0].append(bb_list_msg.boxes[0])
+            self.bb_msg_buf[1].append(my_time)
+        else:
+            self.bb_msg_buf[0][0:self.bb_msg_buf_maxlen] = self.bb_msg_buf[0][1:self.bb_msg_buf_maxlen]
+            self.bb_msg_buf[1][0:self.bb_msg_buf_maxlen] = self.bb_msg_buf[1][1:self.bb_msg_buf_maxlen]
+            self.bb_msg_buf[0][-1] = bb_list_msg.boxes[0]
+            self.bb_msg_buf[1][-1] = my_time
 
 
 if __name__ == '__main__':
