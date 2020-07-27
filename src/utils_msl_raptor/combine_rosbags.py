a10013223874cc36a5e04daf494b0d5d6be401d0
Adam <adam.w.caccavale@gmail.com>
2020-07-27 23:42:25 +0000

    added the combine rosbag code to the slam_test branch. made small tweaks as well to fix timestamp issue

---
+++ src/utils_msl_raptor/combine_rosbags.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# IMPORTS
+# system
+import sys, os, time
+from copy import copy
+from collections import defaultdict
+import yaml
+import pdb
+# math
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+
+from matplotlib import pyplot as plt
+# ros
+import rosbag
+import rospy
+from geometry_msgs.msg import PoseStamped, Twist, Pose
+from sensor_msgs.msg import Image, CameraInfo
+from std_msgs.msg import Float32MultiArray, MultiArrayDimension
+from msl_raptor.msg import AngledBbox, AngledBboxes, TrackedObjects, TrackedObject
+import tf
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+# Utils
+# sys.path.append('/root/msl_raptor_ws/src/msl_raptor/src/utils_msl_raptor')
+# from ssp_utils import *
+# from math_utils import *
+# from ros_utils import *
+# from ukf_utils import *
+# from raptor_logger import *
+# from pose_metrics import *
+# from viz_utils import *
+
+class rosbag_combiner:
+    def __init__(self, rb_path, rb_names, rb_namespaces, ego_ns, rb_out_name):
+        bag_out = rosbag.Bag(rb_path + rb_out_name, 'w')
+        gt_pose_msgs = []  # ns + mavros/vision_pose/pose
+        est_pose_msgs = [] # ns + mavros/local_position/pose
+        tracked_objs = TrackedObjects()
+        all_bb_data = AngledBboxes()
+        for rb_name, rb_ns in zip(rb_names, rb_namespaces):
+            print("processing {} in ns {}".format(rb_name, rb_ns))
+            topics = ["/" + rb_ns + "/mavros/local_position/pose", 
+                      "/" + rb_ns + "/mavros/vision_pose/pose", 
+                      "/" + ego_ns + "/msl_raptor_state",
+                      "/" + ego_ns + "/bb_data",
+                      "/" + ego_ns + "/camera/camera_info",
+                      "/" + ego_ns + "/camera/image_raw",
+                      "/" + ego_ns + "/mavros/vision_pose/pose",
+                      "/" + ego_ns + "/mavros/local_position/pose"]
+            
+            bag_in = rosbag.Bag(rb_path + rb_name, 'r')
+            for topic, msg, t in bag_in.read_messages(topics=topics):
+                # print(t)
+                if topic.split('/')[-1] == "msl_raptor_state":
+                    bag_out.write("/" + ego_ns + "/msl_raptor_state", msg, t)
+                elif topic.split('/')[-1] == "bb_data":
+                    bag_out.write("/" + ego_ns + "/bb_data", msg, t)
+                else:
+                    bag_out.write(topic, msg, t)
+            bag_in.close()
+            b_first_bag = False
+        bag_out.close()
+
+
+        bag_out = rosbag.Bag(rb_path + rb_out_name, 'r')
+        for topic, msg, t in bag_out.read_messages():
+            print("time: {}, topic: {}".format(t, topic))
+        bag_out.close()
+
+
+if __name__ == '__main__':
+    try:
+        np.set_printoptions(linewidth=160, suppress=True)  # format numpy so printing matrices is more clear
+        # program = rosbags_to_logs(rb_name=my_rb_name, data_source=my_data_source, ego_yaml=my_ego_yaml, ado_yaml=my_ado_yaml, b_save_3dbb_imgs=my_b_save_3dbb_imgs)
+        rb_path = "/mounted_folder/raptor_processed_bags/nocs_test/"
+        rb_names = [
+                    # "msl_raptor_output_from_bag_scene_1_bowl_white_small_norm.bag",
+                    "msl_raptor_output_from_bag_scene_1_camera_canon_len_norm.bag",
+                    # "msl_raptor_output_from_bag_scene_1_can_arizona_tea_norm.bag",
+                    "msl_raptor_output_from_bag_scene_1_laptop_air_xin_norm.bag",
+                    "msl_raptor_output_from_bag_scene_1_mug_daniel_norm.bag"]
+        rb_namespaces = [
+                        # "bowl_white_small_norm",
+                        "camera_canon_len_norm",
+                        # "can_arizona_tea_norm",
+                        "laptop_air_xin_norm",
+                        "mug_daniel_norm"]
+        ego_ns = "quad7"
+        rb_out_name = "msl_raptor_output_from_bag_scene_1_merged.bag"
+
+        rb_comb = rosbag_combiner(rb_path, rb_names, rb_namespaces, ego_ns, rb_out_name)
+    except:
+        import traceback
+        traceback.print_exc()
+    print("done with program!")
