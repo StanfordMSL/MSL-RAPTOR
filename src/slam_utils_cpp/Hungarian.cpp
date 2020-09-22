2cd2bbc8626d46b6c6d89b8ecca7d015fea2f3a5
Adam <adam.w.caccavale@gmail.com>
2020-09-22 06:02:58 +0000

    attempted  to include some true hungarian algo code but ran into linking error

---
+++ src/slam_utils_cpp/Hungarian.cpp
@@ -0,0 +1,210 @@
+///////////////////////////////////////////////////////////////////////////////
+// Hungarian.cpp: Implementation file for Class HungarianAlgorithm.
+// 
+// This is a C++ wrapper with slight modification of a hungarian algorithm implementation by Markus Buehren.
+// The original implementation is a few mex-functions for use in MATLAB, found here:
+// http://www.mathworks.com/matlabcentral/fileexchange/6543-functions-for-the-rectangular-assignment-problem
+// 
+// Both this code and the orignal code are published under the BSD license.
+// by Cong Ma, 2016
+// 
+
+// Copyright (c) 2016, mcximing
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// https://github.com/mcximing/hungarian-algorithm-cpp
+
+#include <stdlib.h>
+#include <cfloat> // for DBL_MAX
+#include <cmath>  // for fabs()
+#include "Hungarian.h"
+
+
+HungarianAlgorithm::HungarianAlgorithm(){}
+HungarianAlgorithm::~HungarianAlgorithm(){}
+
+
+//********************************************************//
+// A single function wrapper for solving assignment problem.
+//********************************************************//
+double HungarianAlgorithm::Solve(vector <vector<double> >& DistMatrix, vector<int>& Assignment)
+{
+	unsigned int nRows = DistMatrix.size();
+	unsigned int nCols = DistMatrix[0].size();
+
+	double *distMatrixIn = new double[nRows * nCols];
+	int *assignment = new int[nRows];
+	double cost = 0.0;
+
+	// Fill in the distMatrixIn. Mind the index is "i + nRows * j".
+	// Here the cost matrix of size MxN is defined as a double precision array of N*M elements. 
+	// In the solving functions matrices are seen to be saved MATLAB-internally in row-order.
+	// (i.e. the matrix [1 2; 3 4] will be stored as a vector [1 3 2 4], NOT [1 2 3 4]).
+	for (unsigned int i = 0; i < nRows; i++)
+		for (unsigned int j = 0; j < nCols; j++)
+			distMatrixIn[i + nRows * j] = DistMatrix[i][j];
+	
+	// call solving function
+	assignmentoptimal(assignment, &cost, distMatrixIn, nRows, nCols);
+
+	Assignment.clear();
+	for (unsigned int r = 0; r < nRows; r++)
+		Assignment.push_back(assignment[r]);
+
+	delete[] distMatrixIn;
+	delete[] assignment;
+	return cost;
+}
+
+
+//********************************************************//
+// Solve optimal solution for assignment problem using Munkres algorithm, also known as Hungarian Algorithm.
+//********************************************************//
+void HungarianAlgorithm::assignmentoptimal(int *assignment, double *cost, double *distMatrixIn, int nOfRows, int nOfColumns)
+{
+	double *distMatrix, *distMatrixTemp, *distMatrixEnd, *columnEnd, value, minValue;
+	bool *coveredColumns, *coveredRows, *starMatrix, *newStarMatrix, *primeMatrix;
+	int nOfElements, minDim, row, col;
+
+	/* initialization */
+	*cost = 0;
+	for (row = 0; row<nOfRows; row++)
+		assignment[row] = -1;
+
+	/* generate working copy of distance Matrix */
+	/* check if all matrix elements are positive */
+	nOfElements = nOfRows * nOfColumns;
+	distMatrix = (double *)malloc(nOfElements * sizeof(double));
+	distMatrixEnd = distMatrix + nOfElements;
+
+	for (row = 0; row<nOfElements; row++)
+	{
+		value = distMatrixIn[row];
+		if (value < 0)
+			cerr << "All matrix elements have to be non-negative." << endl;
+		distMatrix[row] = value;
+	}
+
+
+	/* memory allocation */
+	coveredColumns = (bool *)calloc(nOfColumns, sizeof(bool));
+	coveredRows = (bool *)calloc(nOfRows, sizeof(bool));
+	starMatrix = (bool *)calloc(nOfElements, sizeof(bool));
+	primeMatrix = (bool *)calloc(nOfElements, sizeof(bool));
+	newStarMatrix = (bool *)calloc(nOfElements, sizeof(bool)); /* used in step4 */
+
+	/* preliminary steps */
+	if (nOfRows <= nOfColumns)
+	{
+		minDim = nOfRows;
+
+		for (row = 0; row<nOfRows; row++)
+		{
+			/* find the smallest element in the row */
+			distMatrixTemp = distMatrix + row;
+			minValue = *distMatrixTemp;
+			distMatrixTemp += nOfRows;
+			while (distMatrixTemp < distMatrixEnd)
+			{
+				value = *distMatrixTemp;
+				if (value < minValue)
+					minValue = value;
+				distMatrixTemp += nOfRows;
+			}
+
+			/* subtract the smallest element from each element of the row */
+			distMatrixTemp = distMatrix + row;
+			while (distMatrixTemp < distMatrixEnd)
+			{
+				*distMatrixTemp -= minValue;
+				distMatrixTemp += nOfRows;
+			}
+		}
+
+		/* Steps 1 and 2a */
+		for (row = 0; row<nOfRows; row++)
+			for (col = 0; col<nOfColumns; col++)
+				if (fabs(distMatrix[row + nOfRows*col]) < DBL_EPSILON)
+					if (!coveredColumns[col])
+					{
+						starMatrix[row + nOfRows*col] = true;
+						coveredColumns[col] = true;
+						break;
+					}
+	}
+	else /* if(nOfRows > nOfColumns) */
+	{
+		minDim = nOfColumns;
+
+		for (col = 0; col<nOfColumns; col++)
+		{
+			/* find the smallest element in the column */
+			distMatrixTemp = distMatrix + nOfRows*col;
+			columnEnd = distMatrixTemp + nOfRows;
+
+			minValue = *distMatrixTemp++;
+			while (distMatrixTemp < columnEnd)
+			{
+				value = *distMatrixTemp++;
+				if (value < minValue)
+					minValue = value;
+			}
+
+			/* subtract the smallest element from each element of the column */
+			distMatrixTemp = distMatrix + nOfRows*col;
+			while (distMatrixTemp < columnEnd)
+				*distMatrixTemp++ -= minValue;
+		}
+
+		/* Steps 1 and 2a */
+		for (col = 0; col<nOfColumns; col++)
+			for (row = 0; row<nOfRows; row++)
+				if (fabs(distMatrix[row + nOfRows*col]) < DBL_EPSILON)
+					if (!coveredRows[row])
+					{
+						starMatrix[row + nOfRows*col] = true;
+						coveredColumns[col] = true;
+						coveredRows[row] = true;
+						break;
+					}
+		for (row = 0; row<nOfRows; row++)
+			coveredRows[row] = false;
+
+	}
+
+	/* move to step 2b */
+	step2b(assignment, distMatrix, starMatrix, newStarMatrix, primeMatrix, coveredColumns, coveredRows, nOfRows, nOfColumns, minDim);
+
+	/* compute cost and remove invalid assignments */
+	computeassignmentcost(assignment, cost, distMatrixIn, nOfRows);
+
+	/* free allocated memory */
+	free(distMatrix);
+	free(coveredColumns);
+	free(coveredRows);
+	free(starMatrix);
+	free(primeMatrix);
+	free(newStarMatrix);
+
+	return;
+}
