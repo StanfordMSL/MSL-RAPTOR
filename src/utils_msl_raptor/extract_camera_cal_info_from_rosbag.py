9faf0b17f89bd65a1190db83e8817ecae24af522
Adam <adam.w.caccavale@gmail.com>
2020-12-09 07:27:55 +0000

    added code to do camera extrinsic calibration as well THERE IS A BUG!!

--- src/utils_msl_raptor/extract_camera_cal_info_from_rosbag.py
+++ src/utils_msl_raptor/extract_camera_cal_info_from_rosbag.py
@@ -22,6 +22,7 @@ import tf
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from ros_utils import *
+from math_utils import *
 
 class extract_camera_cal_info_from_rosbag:
     def __init__(self, rb_path_and_name, camera_image_topic="/camera/image_raw", ego_pose_topic=None, cal_board_topic=None):
@@ -37,15 +38,55 @@ class extract_camera_cal_info_from_rosbag:
                 ego_pose_time_dict[time] = msg
             elif cal_board_topic and topic == cal_board_topic:
                 cal_board_time_dict[time] = msg
-            elif topic == "/quad7/camera/camera_info":
-                pdb.set_trace()
         
         print("done reading in bag, now syncing times")
-        pdb.set_trace()
-
         
         cv_bridge = CvBridge()
         all_data = {}
+        # MANUALLY ENTERED:
+        used_imgs = [0, 86, 97, 169, 239, 260, 378, 414, 693, 757, 830, 921, 964, 981, 1074]
+        t_cam_cal_arr = np.array([
+                                [-204.57, -217.68,  990.03],
+                                [-191.24, -335.98,  845.45],
+                                [-49.576, -132.26,   936.3],
+                                [-511.24, -54.962,  824.34],
+                                [-291.34,  145.66,  822.04],
+                                [-282.89, -295.94,  931.88],
+                                [232.22,  -389.8,   893.8],
+                                [-203.1,  93.956,  935.06],
+                                [-486.3,  -264.3,  1343.1],
+                                [-191.82,  115.32,  763.79],
+                                [-241.55, -126.74,  709.89],
+                                [28.146,  -34.15,  882.53],
+                                [-371.2, -19.621,  503.97],
+                                [-85.631, -129.23,  686.26],
+                                [-278.61,  50.078,   517.1]]) / 1000.0
+        rot_vec_cam_cal_arr = np.array([[0.001816,  0.026344, 0.0031563],
+                                [0.15452,  0.027444, -0.010806],
+                                [0.43315,  0.3572,  .18638],
+                                [0.48199,  -0.63426,  -0.13182],
+                                [0.20023,  -0.51965,  -0.17459],
+                                [0.51043,  .32159,  -0.25896],
+                                [0.69905,  .55085,   0.736],
+                                [0.027512,  .26331,  0.021262],
+                                [0.53707,  -0.45017,  -0.12076],
+                                [0.37872,  .08069, -0.041002],
+                                [0.21917,  .10182, 0.0089506],
+                                [0.42675,  .79288, -0.044611],
+                                [0.5388,  -0.27856, -0.039843],
+                                [0.21748,  0.2524,  0.040668],
+                                [0.41918,  0.5665,  -0.18436]])
+        chosen_pic_ind = 0
+        t_ego_cam_ave = np.zeros((3,))
+        N = rot_vec_cam_cal_arr.shape[0]
+        quat_ego_cam = np.zeros((N,4))
+        t_w_cal_offset = np.asarray([0, -7*40.0/1000.0, 4.5*40.0/1000.0])  # matlab assumes calibration board axis is in bottom right corner, each square is 40mm and board is 9 x 14
+        R_w_cal_offset = np.array([[ 0.,  0.,  1.],
+                                   [ 0., -1.,  0.],
+                                   [ 1.,  0.,  0.]])  # FOR SOME REASON THIS IS NEEDED???
+        R_cal_calmatlab = np.array([[ 0.,  0.,  1.],
+                                    [ 1.,  0.,  0.],
+                                    [ 0.,  1.,  0.]])  # matlabs x is our y, matlabs y is our z, matlabs z is our x
         for img_ind, img_time in enumerate(image_time_dict):
             image = cv_bridge.imgmsg_to_cv2(image_time_dict[img_time], desired_encoding="passthrough")
             # image = cv_bridge.imgmsg_to_cv2(image_time_dict[img_time], desired_encoding="bgr8")
@@ -53,7 +94,66 @@ class extract_camera_cal_info_from_rosbag:
             closest_cal_board_time, _ = find_closest_by_time(img_time, [*cal_board_time_dict.keys()])  
             all_data[(img_time + closest_ego_time + closest_cal_board_time) / 3.0] = (image, ego_pose_time_dict[closest_ego_time], cal_board_time_dict[closest_cal_board_time])
             fn_str = "cal_img_{}".format(img_ind)
-            cv2.imwrite(('/').join(rb_path_and_name.split('/')[:-1]) + '/' + fn_str + ".jpg", image)
+            if 0:
+                cv2.imwrite(('/').join(rb_path_and_name.split('/')[:-1]) + '/' + fn_str + ".jpg", image)
+            
+            if img_ind in used_imgs:
+                t_w_ego = np.array([ego_pose_time_dict[closest_ego_time].pose.position.x, ego_pose_time_dict[closest_ego_time].pose.position.y, ego_pose_time_dict[closest_ego_time].pose.position.z])
+                t_w_cal = np.array([cal_board_time_dict[closest_cal_board_time].pose.position.x, cal_board_time_dict[closest_cal_board_time].pose.position.y, cal_board_time_dict[closest_cal_board_time].pose.position.z])
+                t_w_cal -= t_w_cal_offset
+                R_w_ego = quat_to_rotm(np.array([ego_pose_time_dict[closest_ego_time].pose.orientation.w,
+                                                ego_pose_time_dict[closest_ego_time].pose.orientation.x,
+                                                ego_pose_time_dict[closest_ego_time].pose.orientation.y,
+                                                ego_pose_time_dict[closest_ego_time].pose.orientation.z]))
+                R_w_cal = quat_to_rotm(np.array([cal_board_time_dict[closest_cal_board_time].pose.orientation.w,
+                                                cal_board_time_dict[closest_cal_board_time].pose.orientation.x,
+                                                cal_board_time_dict[closest_cal_board_time].pose.orientation.y,
+                                                cal_board_time_dict[closest_cal_board_time].pose.orientation.z]))
+                R_w_cal = R_w_cal @ R_w_cal_offset  # NOT SURE WHY THIS IS NEEDED
+                R_w_cal = R_w_cal @ R_cal_calmatlab # this is due to a difference in how matlab defines its calibration axis (https://www.mathworks.com/help/vision/ref/showextrinsics.html#btykfmm-1-view)
+                tf_w_ego = np.eye(4)
+                tf_w_ego[0:3, 0:3] = R_w_ego
+                tf_w_ego[0:3, 3] = t_w_ego
+                tf_w_cal = np.eye(4)
+                tf_w_cal[0:3, 0:3] = R_w_cal
+                tf_w_cal[0:3, 3] = t_w_cal
+                tf_ego_cal = inv_tf(tf_w_ego) @ tf_w_cal
+                # print(tf_w_cal)
+                # print("image {} t / R: {}, {}, {} ||\n{}, {}, {}\n{}, {}, {}\n{}, {}, {}\n".format(img_ind, ))
+                t_cam_cal = t_cam_cal_arr[chosen_pic_ind, :]
+                ax_ang_cam_cal = rot_vec_cam_cal_arr[chosen_pic_ind, :]  # this is ax/ang format where |vec| = ang, vec/|vec| is axis
+                R_cam_cal = quat_to_rotm(axang_to_quat(ax_ang_cam_cal)[0])[0].T
+                tf_cam_cal = np.eye(4)
+                tf_cam_cal[0:3, 0:3] = R_cam_cal
+                tf_cam_cal[0:3, 3] = t_cam_cal
+                tf_ego_cam = tf_ego_cal @ inv_tf(tf_cam_cal)
+                t_ego_cam_ave += tf_ego_cam[0:3, 3]
+                quat_ego_cam[chosen_pic_ind, :] = rotm_to_quat(tf_ego_cam[0:3, 0:3])
+                print(tf_ego_cam)
+                pdb.set_trace()
+                chosen_pic_ind += 1
+        t_ego_cam_ave /= N
+        quat_ego_cam_ave, _ = average_quaternions(Q=quat_ego_cam, w=None)
+
+        tf_ego_cam_final = np.eye(4)
+        tf_ego_cam_final[0:3, 0:3] = quat_to_rotm(quat_ego_cam_ave)
+        tf_ego_cam_final[0:3, 3] = t_ego_cam_ave
+        print("\n\nFINAL EXTRINSICS CALIBRATION RESULT:\n{}".format(tf_ego_cam_final))
+
+        tf_ego_cam_CAD = np.eye(4)
+        # tf_ego_cam_CAD[0:3, 0:3] = np.array([[ 0., -1.,  0.],
+        #                                      [ 0.,  0., -1.],
+        #                                      [ 1.,  0.,  0.]])
+        tf_ego_cam_CAD[0:3, 0:3] = np.array([[ 0., -1.,  0.],
+                                             [ 0.,  0., -1.],
+                                             [ 1.,  0.,  0.]]).T
+        tf_ego_cam_CAD[0:3, 3] = [0.13854437, -0.01504337, -0.06380886]
+        print("\n(expected result)\n{}".format(tf_ego_cam_CAD))
+        pdb.set_trace()
+        # Adapted from https://github.com/christophhagen/averaging-quaternions/blob/master/averageQuaternions.py
+        # Q is a Nx4 numpy matrix and contains the quaternions to average in the rows.
+        # The quaternions are arranged as (w,x,y,z)
+
         print("done syncing data")
         bag_in.close()
 
